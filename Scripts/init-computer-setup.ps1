cb1f9808d49ca71ce23a97e56c6141b87c9f8e3b
Joseph Davis <davisj38@michigan.gov>
2025-01-02 14:40:30 -0500

    Update initial computer setup script

--- Scripts/init-computer-setup.ps1
+++ Scripts/init-computer-setup.ps1
@@ -2,4 +2,8 @@
 Get-WindowsCapability -Name RSAT.ActiveDirectory* -Online | Add-WindowsCapability -Online
 
 # To verify install
-Get-WindowsCapability -Name RSAT* -Online | Select-Object -Property DisplayName, State
+# Get-WindowsCapability -Name RSAT* -Online | Select-Object -Property DisplayName, State
+
+
+# Windows 11 - Remove default right-click menu
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
