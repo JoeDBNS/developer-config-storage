4406e4368261633712cf22de7a53a1595104e9a1
Joseph Davis <davisj38@michigan.gov>
2025-01-02 13:00:59 -0500

    Add to and update the AD scripts

--- Scripts/ad-work.ps1
+++ Scripts/ad-work.ps1
@@ -2,10 +2,16 @@
 Get-WindowsCapability -Online | Where-Object { $_.Name -like "Rsat.ActiveDirectory.DS-LDS.Tools*" } | Add-WindowsCapability -Online
 
 
+# Search for User by SOM name
+Get-ADUser -Filter 'SamAccountName -like "*__username__*"'
+
 # Search for group by name
 Get-ADGroup -Filter {Name -like 'ES_SADLC_*'}
 
 # Get Groups associated with User
-Get-ADPrincipalGroupMembership davisj38 | Select-Object Name
+Get-ADPrincipalGroupMembership __username__ | Select-Object Name
 # or
-Get-ADUser davisj38 -Properties memberof | Select-Object -Expand memberof
+Get-ADUser __username__ -Properties memberof | Select-Object -Expand memberof
+
+# Get Accounts that are part of a Group
+Get-ADGroupMember -Identity __group__ | Select-Object SamAccountName
